226721e2bc7803a1cbd4607d7f54a672b29475ba
osdal <osdal67@gmail.com>
2018-09-04 01:48:30 +0300

    ready

---
+++ js/bundle/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+
+let form = require('../parts/form.js');
+let contactForm = require('../parts/contactForm.js');
+let tab = require('../parts/tab.js');
+let modal = require('../parts/modal.js');
+let slider = require('../parts/slider.js');
+let calc = require('../parts/calc.js');
+let timer = require('../parts/timer.js');
+
+tab();	
+form();
+modal();
+timer();	
+calc();
+slider();
+contactForm();
+
+
+
+
+});
+
